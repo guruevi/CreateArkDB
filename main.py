873961786144ce708db0ae36308a9003a0343a3d
Evi Vanoost <vanooste@rcbi.rochester.edu>
2022-11-03 19:58:35 -0400

    Cleanup

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import json
 import os
-import re
 
 from pymongo import MongoClient
 
 # Setup MongoDB connection
-mongo_client = MongoClient('mongodb://ansible:S%25Rqs5mECnd3%5ES@nsc-docker1:27017/ansible')
+mongo_client = MongoClient('mongodb://user:pass@server:27017/')
 db = mongo_client['ansible']
 collection = db['intel_ark']
 
@@ -38,14 +37,3 @@ def find_files(path_to_json):
 
 if __name__ == '__main__':
     find_files("./items")
-
-# Regex to find
-string1 = "Intel(R) Core(TM) i7-9700 CPU @ 3.00GHz" # Find i7-9700
-string2 = "Intel(R) Core(TM) i5-12500 CPU" # Find i5-12500
-string3 = "Intel(R) Xeon(R) Gold 5215 Processor" # Find 5215
-regex = re.compile(r"(?:i\d-\d{4,5}|\d{4})")
-
-# Match regex to string
-match1 = regex.search(string1)
-match2 = regex.search(string2)
-match3 = regex.search(string3)
